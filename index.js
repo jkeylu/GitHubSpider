89d19c3c1a2b304355acdf933196ee9d1aeebb46
jKey Lu <jkeylu@gmail.com>
2015-04-06 20:51:26 +0800

    fixed bugs

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
 var mongodb = require('mongodb')
   , elasticsearch = require('elasticsearch')
+  , needle = require('needle')
   , tokens = require('./lib/tokens')
   , GithubApi = require('./lib/github_api')
   , Db = require('./lib/db')
   , Spider = require('./lib/spider')
-  , config = require('./config.json');
+  , config = require('./config.json')
+  , package = require('./package.json');
+
+var userAgent = package.name + '/' + package.version;
+needle.defaults({
+  accept: 'application/vnd.github.v3+json',
+  user_agent: userAgent
+});
 
 var tokenManager = new tokens.TokenManager();
 tokenManager.init(config.tokens);
 
-var es = new elasticsearch.Client(config.elasticsearch);
 var githubApi = new GithubApi(tokenManager);
 
 var app = {
   config: config,
   tokenManager: tokenManager,
-  es: es,
   githubApi: githubApi
 };
+
+if (app.config.runElasticsearchWorker) {
+  app.es = new elasticsearch.Client(config.elasticsearch);
+}
 
 mongodb.MongoClient.connect(config.mongodbUrl, function(err, database) {
   if (err) {
